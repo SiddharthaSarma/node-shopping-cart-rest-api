61dcb4a567e6d0a7807a1cf512ba434073b62d76
siddharthasarma <sidduinevitable@gmail.com>
2018-02-03 19:27:38 +0530

    Added morgan

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { productRoutes } from './api/routes/products';
 import { orderRoutes } from './api/routes/orders';
+import morgan from 'morgan';
 export const app = express();
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
