0b2c97c032d29a8c030acddac7eb5dac4190ab78
siddharthasarma <sidduinevitable@gmail.com>
2018-02-03 19:28:14 +0530

    Added error handling

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message
+  });
+});
