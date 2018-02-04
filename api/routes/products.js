d87bdce8583c3dd149eb7f34e26118018e8d5cb8
siddharthasarma <sidduinevitable@gmail.com>
2018-02-04 17:29:17 +0530

    Integrated products with mongo

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,20 +1,53 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { Product } from '../models/product';
 export const productRoutes = express.Router();
 
 productRoutes.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Get request for the products'
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      if (docs.length) {
+        res.status(200).json({ products: docs });
+      } else {
+        res.status(404).json({ message: 'No products found' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 productRoutes.post('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Post request for the products'
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+  product
+    .save()
+    .then(result => {
+      res.status(200).json({
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 productRoutes.get('/:productId', (req, res, send) => {
-  res.status(200).json({
-    message: 'Get request for individual product'
-  });
+  const id = req.params.productId;
+  Product.findById(id)
+    .then(product => {
+      res.status(200).json(product);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
