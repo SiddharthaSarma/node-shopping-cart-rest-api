1c38b0d826370a4fc74d9453f4fb409f83938519
siddharthasarma <sidduinevitable@gmail.com>
2018-02-04 08:48:22 +0530

    Added CORS

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
+    return res.send(200).json({});
+  }
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
